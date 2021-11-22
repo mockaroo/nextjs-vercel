94bd81c440877671b85f0e77c0e1408769957678
Mark Brocato <mark.brocato@moovweb.com>
2021-11-22 13:13:56 +0200

    Add auth

--- pages/_middlware.ts
+++ pages/_middlware.ts
@@ -1,21 +1,39 @@
-import { NextRequest, NextResponse } from 'next/server'
+// import { NextRequest, NextResponse } from 'next/server'
+
+// export default function middleware(req: NextRequest) {
+//   const basicAuth = req.headers.get('authorization')
+
+//   if (basicAuth) {
+//     const auth = basicAuth.split(' ')[1]
+//     const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+
+//     if (user === 'user' && pwd === 'password') {
+//       return NextResponse.next()
+//     }
+//   }
+
+//   return new Response('Auth required', {
+//     status: 401,
+//     headers: {
+//       'WWW-Authenticate': 'Basic realm="Secure Area"',
+//     },
+//   })
+// }
+
+import type { NextRequest } from 'next/server';
+
+// Block GB, prefer US
+const BLOCKED_COUNTRY = 'GB';
 
 export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get('authorization')
+  const country = req.geo.country || 'US';
 
-  if (basicAuth) {
-    const auth = basicAuth.split(' ')[1]
-    const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
-
-    if (user === 'user' && pwd === 'password') {
-      return NextResponse.next()
-    }
+  // If the request is from the blocked country,
+  // send back a response with a status code
+  if (country === BLOCKED_COUNTRY) {
+    return new Response('Blocked for legal reasons', { status: 451 });
   }
 
-  return new Response('Auth required', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Secure Area"',
-    },
-  })
+  // Otherwise, send a response with the country
+  return new Response(`Greetings from ${country}, where you are not blocked.`);
 }
